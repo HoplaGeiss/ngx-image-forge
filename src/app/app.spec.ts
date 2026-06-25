589c39f6e75f8d29d0131c8e5de265a4a88de5e0
Gabriel Muller <gabriel.muller.12@gmail.com>
2026-06-25 22:05:38 +0200

    fix: UT

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ngx-image-forge');
+    expect(compiled.querySelector('h1')?.textContent).toContain('ngx-image-forge');
   });
 });
